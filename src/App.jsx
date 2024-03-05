07ca9408d7344d12bd79283e54c5b031d2b1c03a
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-03-04 21:11:25 -0300

    se creó el componente TituloSecundario

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import TituloSecundario from "./components/TituloSecundario";
 
 function App() {
 
   return (
-    <>
+    <section className="container">
+      <h1 className="display-3 text-center my-3">Contador con React</h1>
+      <TituloSecundario></TituloSecundario>
       
-    </>
+    </section>
   )
 }
 
---
+++ src/components/TituloSecundario.jsx
@@ -0,0 +1,14 @@
+const TituloSecundario = () => {
+    return (
+        <article>
+            <h2 className="mb-3">Practica con react</h2>
+            <p>
+            React es una biblioteca de JavaScript desarrollada por Facebook para construir interfaces de usuario interactivas y eficientes en aplicaciones web de una sola página. Su enfoque declarativo y su arquitectura basada en componentes facilitan la creación de interfaces reutilizables y escalables. React ha ganado popularidad debido a su rendimiento optimizado y su capacidad para trabajar de manera integrada con otras tecnologías, convirtiéndolo en una herramienta fundamental en el desarrollo moderno de aplicaciones web.
+            <br/>
+            Comision: c73i
+            </p>
+        </article>
+    );
+};
+
+export default TituloSecundario;
