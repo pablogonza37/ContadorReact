8354f972b06cee0d2a49efb5ea365c933c9ffebe
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-03-04 21:19:30 -0300

    se utilizo props para enviar informaciion al componente TituloSecundario

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import TituloSecundario from "./components/TituloSecundario";
 import Contador from "./components/Contador";
 
 function App() {
-
+  const anioActual = 2024;
   return (
     <section className="container">
       <h1 className="display-3 text-center my-3">Contador con React</h1>
-      <TituloSecundario></TituloSecundario>
+      <TituloSecundario comision='c73i' anioActualProps={anioActual}></TituloSecundario>
       <Contador></Contador>
     </section>
   )
